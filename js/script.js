518ef15f3e3ab4fbc4d0e758b3140b67d5fe2016
Rajul <rajulyadav123@gmail.com>
2022-09-17 15:46:24 +0530

    FizzBuzz code implimented beautifully.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,35 @@
-let str = "hello there general kenobi!!!";
+function add7(input)
+{
+    return input + 7;
+}
+function capatialize(input)
+{
+    return input.replace(RegExp(input[0],"g"),input[0].toUpperCase());
+}
+function lastLetter(input)
+{
+    return (input[input.length - 1]);
+}
 
-let substr = str.split("");
+function FizzBuzz(input)
+{
+    if(input % 3 == 0)
+    {
+        return "Fizz";
+    }   
+    else if(input % 5 ==0)
+    {
+        return "Buzz";
+    }
+    else
+    {
+        return undefined;
+    }
+}
 
-substr[0] = 'y';
+let answer = parseInt(prompt("Enter a number to FizzBuzz to: "));
 
-let ass = substr.toString().replace(/,/g,"");
-
-console.log(substr.toString());
-console.log(ass);
+for (let index = 1; index <= answer; index++) 
+{
+    console.log(FizzBuzz(index) ? FizzBuzz(index) : index);
+}
